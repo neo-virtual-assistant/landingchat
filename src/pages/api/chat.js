75c0cdc990a050e8870950f5265c94807502f0b9
superkleber <alejandrofaesta@gmail.com>
2023-04-12 18:55:55 -0400

    olvidé descomentar la API xD

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -7,35 +7,35 @@ export default async function handler(req, res) {
   }
 
   try {
-    // let response;
-    // response = await fetch(`https://ai.kleber.digital/get-answer`, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     key: `${process.env.API_KEY}`,
-    //     prompt: `${prompt}`,
-    //     external_user_id: `${external_user_id}`,
-    //     webhook: "https://n8n.kleber.digital/webhook/discord-kleberai",
-    //   }),
-    // });
+    let response;
+    response = await fetch(`https://ai.kleber.digital/get-answer`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        key: `${process.env.API_KEY}`,
+        prompt: `${prompt}`,
+        external_user_id: `${external_user_id}`,
+        webhook: "https://n8n.kleber.digital/webhook/discord-kleberai",
+      }),
+    });
 
-    // if (response.status == 500) {
-    //   return res.status(500).json({ response: "Error Interno del Servidor" });
-    // }
-    // if (response.status == 429) {
-    //   return res.status(429).json({ response: "Demasiadas solicitudes" });
-    // }
+    if (response.status == 500) {
+      return res.status(500).json({ response: "Error Interno del Servidor" });
+    }
+    if (response.status == 429) {
+      return res.status(429).json({ response: "Demasiadas solicitudes" });
+    }
 
-    // if (response.status !== 200) {
-    //   throw new Error(`Request failed with status ${response.status}`);
-    // }
+    if (response.status !== 200) {
+      throw new Error(`Request failed with status ${response.status}`);
+    }
     
-    // if (response.status == 200) {
-    //   const json = await response.json();
-    //   return res.status(200).json({ response: json });
-    // }
+    if (response.status == 200) {
+      const json = await response.json();
+      return res.status(200).json({ response: json });
+    }
   } catch (e) {
     console.error(e);
     res.status(500).json({ error: e });
